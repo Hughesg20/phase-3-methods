9f2dc44b7d4e1c9188b92490d1cf56ec5bb9f9c0
Griffin Hughes <Griffinhughes2020@gmail.com>
2022-02-28 09:45:58 -0500

    commit tree 573d619f6bf9479b30a2fda39f9bf9f612ee6958

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+    
+end
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+    
+end
+
+def add uno, dos
+   uno + dos
+end
+
+def halve integer 
+    if integer.class == Integer
+    integer / 2
+    end
+end
